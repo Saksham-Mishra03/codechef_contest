c9e10d585d35e206f096fc50b039d8929cc9fd0d
SAKSHAM MISHRA <98811900+Saksham-Mishra03@users.noreply.github.com>
2022-11-16 21:32:12 +0530

    Create Parity.cpp

---
+++ Starters 65/Parity.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,x;
+	cin>>n;
+	while(n--)
+	{cin>>x;
+	    if(x%2==0)
+	    cout<<"yes"<<endl;
+	    else
+	    cout<<"no"<<endl;
+	}
+	return 0;
+}
